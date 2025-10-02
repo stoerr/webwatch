9c8a778661f643dcba64242bc6425241d1bc9fb3
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2025-10-02 22:36:30 +0200

    initial archetype import

---
+++ src/main/java/net/stoerr/tools/App.java
@@ -0,0 +1,13 @@
+package net.stoerr.tools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
