3b684a659715e20488609cb2759749baf55385e2
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2025-10-03 14:19:57 +0200

    executable

--- src/main/java/net/stoerr/tools/App.java
+++ src/main/java/net/stoerr/tools/App.java
@@ -1,13 +1,17 @@
 package net.stoerr.tools;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        if (args != null && args.length > 0 && "--tango".equals(args[0])) {
+            // forward remaining args to PrintTangoConcerts3.main
+            String[] forwarded = Arrays.copyOfRange(args, 1, args.length);
+            PrintTangoConcerts3.main(forwarded);
+        }
     }
 }
